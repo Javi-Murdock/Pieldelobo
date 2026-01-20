5035ed9f644051f015bcd283b4a791ef20d4cc49
Javi-Murdock <javiergalalopez31@gmail.com>
2026-01-20 22:45:16 +0100

    Add files via upload

--- android/src/main/java/io/github/javiergames/pieldelobo/android/AndroidLauncher.java
+++ android/src/main/java/io/github/javiergames/pieldelobo/android/AndroidLauncher.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import io.github.javiergames.pieldelobo.Main;
+import io.github.javiergames.pieldelobo.GestorJuego.Main;
 
 /** Launches the Android application. */
 public class AndroidLauncher extends AndroidApplication {
